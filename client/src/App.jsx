6de89bf572aea8a3e8cb02aaaa08d5bb58dc2f64
zulkiffal <zulkifalaslam123@gmail.com>
2024-11-12 00:54:27 +0500

    add routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,26 @@
-import './App.css'
+import React from "react";
+
+import {  BrowserRouter,  Routes,  Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/about";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 function App() {
 
   return (
-    <>
-      <div>
-   <h1 className="text-3xl font-bold underline text-red-600">
-      Hello world!
-    </h1>
-      </div>
-    </>
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
