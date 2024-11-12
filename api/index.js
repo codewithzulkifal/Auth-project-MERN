288f03dc59f6810360b81f380676bc5e18973e1d
zulkiffal <zulkifalaslam123@gmail.com>
2024-11-12 23:05:48 +0500

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,12 +1,29 @@
-import express from "express"
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
 
 const app = express();
-const port = 4045;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("MONGO-DB CONNECTION SUCCEED !!!");
+  } catch (error) {
+    console.log(`MONGODB CONNECTION FAILED !! : ${error}`);
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("MongoDB Disconnected");
+});
+mongoose.connection.on("connected", () => {
+  console.log("MongoDB Connected");
 });
 
-app.listen(port, () => {
-  console.log(`Server app listening on port ${port}`);
+
+app.listen(9000, () => {
+  connect();
+  console.log("App is listening on port 9000");
 });
