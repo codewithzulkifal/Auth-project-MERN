836e6e097516b4c621ef7b81465463f261197253
zulkiffal <zulkifalaslam123@gmail.com>
2024-11-13 14:39:33 +0500

    update connection of DB

--- api/index.js
+++ api/index.js
@@ -15,15 +15,37 @@ const connect = async () => {
   }
 };
 
-mongoose.connection.on("disconnected", () => {
-  console.log("MongoDB Disconnected");
-});
-mongoose.connection.on("connected", () => {
-  console.log("MongoDB Connected");
-});
+// mongoose.connection.on("disconnected", () => {
+//   console.log("MongoDB Disconnected");
+// });
+// mongoose.connection.on("connected", () => {
+//   console.log("MongoDB Connected");
+// });
+
+app.get('/', (req,res) => {
+    res.json({
+        message: "API"
+    })
+})
+
+// app.listen(9000, () => {
+//   connect();
+//   console.log("App is listening on port 9000");
+// });
 
 
-app.listen(9000, () => {
-  connect();
-  console.log("App is listening on port 9000");
-});
+connect()
+  .then(() => {
+    const port = 9998;
+
+    app.on("error", (error) => {
+      console.log(" ERROR: ", error);
+    });
+
+    app.listen(port, () => {
+      console.log(` App is running on PORT ${port} `);
+    });
+  })
+  .catch((error) => {
+    console.log(" MONGO_DB CONNECT_DB FAILED !!!! ", error);
+  });
